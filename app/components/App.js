f71bc4ff48af4404c55a426d77a9e76597161ba3
Stewart Yu <stewartyu@gmail.com>
2016-01-24 22:22:54 -0800

    let react manage links

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <header>
+                    <Link to="/">Instagram Fight</Link>
+                    <Link to="/popular">Most Popular</Link>
+                </header>
                 {this.props.children}
             </div>
         );
