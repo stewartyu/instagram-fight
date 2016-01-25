906f4189f98812fe8d6be30f18cc362fd685e66c
Stewart Yu <stewartyu@gmail.com>
2016-01-25 00:02:36 -0800

    update redirect_uri

--- instagram/config.js
+++ instagram/config.js
@@ -1,5 +1,5 @@
 var config = {
-    instagram_redirect_uri: 'http://localhost:3000/handleauth',
+    instagram_redirect_uri: 'http://instagram-fight.herokuapp.com/handleauth',
     instagram_client_id: '633013de7720450bb667343b674a8064',
     instagram_client_secret: '6364e84b3d6c45e38b8edeb818cad695',
     database: process.env.MONGOLAB_URI || 'mongodb://localhost/instagramfight'
